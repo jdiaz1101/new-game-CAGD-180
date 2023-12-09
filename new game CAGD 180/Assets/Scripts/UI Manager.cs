90617c09f9e30bfdc341a3bc35a15f808a255b2a
J2ExSlayer <jarroyo1@csuchico.edu>
2023-12-08 19:36:20 -0800

    made a timer

--- new game CAGD 180/Assets/Scripts/UI Manager.cs
+++ new game CAGD 180/Assets/Scripts/UI Manager.cs
@@ -10,11 +10,17 @@ public class UIManager : MonoBehaviour
     public PlayerController pController;
     public TMP_Text pointsText;
     public TMP_Text healthText;
+    public TMP_Text timerText;
+
+    float currentTime = 0f;
+    float startingTime = 60f;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentTime = startingTime;
     }
 
     // Update is called once per frame
@@ -22,5 +28,14 @@ public class UIManager : MonoBehaviour
     {
         pointsText.text = "Points: " + pController.totalPoints;
         healthText.text = "Health: " + pController.health;
+
+        currentTime -= 1 * Time.deltaTime;
+        timerText.text = currentTime.ToString();
+
+        if( currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+
     }
 }
